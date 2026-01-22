bda4bfab85e38d1850a763541f58c846b2287710
Vivek-Kumar-1907 <vivekkkrumar2007@gmail.com>
2026-01-22 16:04:42 +0530

    added functions to webapp

--- calculator/web/main.js
+++ calculator/web/main.js
@@ -1,5 +1,13 @@
 let evalstr = [''];
 let bal = 0;
+let mode = 0;
+
+let modes = 
+    {
+        m1 : ['\u03C0', 'e', '|x|', '\u221A', 'x\u02b8'],
+        m2 : ['sin', 'cos', 'tan', 'ln', 'fac'],
+        m3 : ['arcsin', 'arccos', 'arctan', '\u03C0', 'e'],
+    };
 
 function num(a){
     if(evalstr[evalstr.length - 1] == '' && evalstr[evalstr.length - 2] == ')'){
@@ -21,6 +29,8 @@ function isoperator(a){
             break;
         case '/':return 1;
             break;
+        case '^': return 1;
+            break;
     }
     return 0;
 }
@@ -38,11 +48,15 @@ function addoperator(a){
         return;
     if(evalstr[evalstr.length-1] == '')
         evalstr.pop();
-    if(evalstr[evalstr.length-1] == '(' && a!=3)
+    if(evalstr[evalstr.length-1] == '(' && a!=3){
+        evalstr.push('');
         return;
+    }
     if(isoperator(evalstr[evalstr.length-1])){
-        if(evalstr[evalstr.length-2] == '(' || evalstr.length == 1)
+        if(evalstr[evalstr.length-2] == '(' || evalstr.length == 1){
+            evalstr.push('');
             return;
+        }
         evalstr.pop();
     }
     switch(a){
@@ -58,6 +72,9 @@ function addoperator(a){
         case 4: evalstr.push('+');
                 evalstr.push('');
         break;
+        case 5: evalstr.push('^');
+                evalstr.push('');
+        break;
     }
 }
 
@@ -71,7 +88,7 @@ function addlbrak(){
     }
     if(evalstr[evalstr.length-1] == '')
         evalstr.pop();
-    if(isoperator(evalstr[evalstr.length-1])){
+    if(isoperator(evalstr[evalstr.length-1]) || evalstr[evalstr.length-1] == '('){
         evalstr.push('(');
         evalstr.push('');
     }
@@ -96,22 +113,107 @@ function addrbrak(){
     bal--;
 }
 
+function addconstant(c){
+    if(!isNaN(evalstr[evalstr.length-1])){
+        addoperator(2);
+    }
+    switch(c){
+        case 'pi': num(3.141592);
+        break;
+        case 'e': num(2.71828);
+        break;
+    }
+}
+
+function addfunc(f){
+    if(evalstr.length == 1 && evalstr[0] == ''){
+        evalstr.pop();
+        evalstr.push(f);
+        evalstr.push('(');
+        evalstr.push('');
+        bal++;
+        return;
+    }
+    if(evalstr[evalstr.length-1] == '')
+        evalstr.pop();
+    if(isoperator(evalstr[evalstr.length-1]) || evalstr[evalstr.length-1] == '('){
+        evalstr.push(f);
+        evalstr.push('(');
+        evalstr.push('');
+    }
+    else{
+        evalstr.push('*');
+        evalstr.push(f);
+        evalstr.push('(');
+        evalstr.push('');
+    }
+    bal ++;
+}
+
+function isfunc(f){
+     switch(f){
+        case 'mod':return 1;
+            break;
+        case 'root':return 1;
+            break;
+        case 'sin':return 1;
+            break;
+        case 'cos':return 1;
+            break;
+        case 'tan': return 1;
+            break;
+        case 'arcsin': return 1;
+             break;
+        case 'arccos': return 1;
+             break;
+        case 'arctan': return 1;
+             break;
+        case 'ln': return 1;
+             break;
+        case 'arccos': return 1;
+             break;
+        case 'arctan': return 1;
+             break;
+        case 'fac': return 1;
+             break;
+    }
+    return 0; 
+}
+
+function setmode(md){
+    let setstrings = [];
+    switch(md){
+        case 0: setstrings = modes.m1;
+        break;
+        case 1: setstrings = modes.m2;
+        break;
+        case 2: setstrings = modes.m3;
+        break;
+    }
+    for(let i = 0; i<5; i++)
+        $('#f' + (i+1)).text(setstrings[i]);
+}
+
 $("#clear").click(function(){
+    if(!isNaN(evalstr[evalstr.length-1]) && evalstr[evalstr.length - 1] > 1){
+        evalstr[evalstr.length-1] = evalstr[evalstr.length-1].substring(0, evalstr[evalstr.length - 1].length - 1);
+        return;
+    }
     if(evalstr[evalstr.length-1] == '')
         evalstr.pop();
     evalstr.pop();
+    if(isfunc(evalstr[evalstr.length-1]))
+        evalstr.pop();
     if(isoperator(evalstr[evalstr.length-1]) || evalstr[evalstr.length-1] == ')' || evalstr[evalstr.length-1] == '('){
         evalstr.push('');
     }
     if(evalstr.length == 0){
         evalstr = [''];
     }
-    console.log(evalstr);
 });
 
 $('#clear').dblclick(function(){
     evalstr = [''];
-    console.log(evalstr);
 });
 
 for(let i=0;i<=9;i++){
@@ -139,3 +241,106 @@ for(let i = 1; i<=4; i++){
 
 $('#lbrak').click(()=>{addlbrak();});
 $('#rbrak').click(()=>{addrbrak();});
+
+$('#switch').click(()=>{
+    mode ++;
+    if(mode > 2)
+        mode = 0;
+    setmode(mode);
+})
+
+$('#f1').click(()=>{
+    switch(mode){
+        case 0: addconstant('pi');
+        break;
+        case 1: addfunc('sin');
+        break;
+        case 2: addfunc('arcsin');
+        break;
+    }
+});
+
+$('#f2').click(()=>{
+    switch(mode){
+        case 0: addconstant('e');
+        break;
+        case 1: addfunc('cos');
+        break;
+        case 2: addfunc('arccos');
+        break;
+    }
+});
+
+$('#f3').click(()=>{
+    switch(mode){
+        case 0: addfunc('mod');
+        break;
+        case 1: addfunc('tan');
+        break;
+        case 2: addfunc('arctan');
+    }
+});
+
+$('#f4').click(()=>{
+    switch(mode){
+        case 0: addfunc('root');
+        break;
+        case 1: addfunc('ln');
+        break;
+        case 2: addconstant('pi');
+        break;
+    }
+});
+
+$('#f5').click(()=>{
+    switch(mode){
+        case 0: addoperator(5);
+        break;
+        case 1: addfunc('fac');
+        break;
+        case 2: addconstant('e');
+        break;
+    }
+});
+
+$('#eq').click(()=>{
+    let itr = 10000;
+    let itr2 = 10000;
+    if(bal > 0){
+        while(bal > 0 && itr > 0){
+            if(evalstr[evalstr.length-1] == ''){
+                    evalstr.pop();
+            }
+            while((isNaN(evalstr[evalstr.length-1]) && evalstr[evalstr.length - 1] != ')') && itr2 > 0){
+                if(isoperator(evalstr[evalstr.length-1])){
+                    evalstr.pop();
+                }
+                if(evalstr[evalstr.length - 1] == '('){
+                    evalstr.pop();
+                    bal --;
+                }
+                if(isfunc(evalstr[evalstr.length - 1])){
+                    evalstr.pop();
+                }
+                itr2 --;
+            }
+            if(bal > 0){
+                addrbrak();
+                bal ++;
+                bal --;
+            }
+            itr --;
+        }
+    }
+    let res = '';
+    for(let i = 0; i<evalstr.length; i++){
+        res += evalstr[i];
+        res += ' ';
+    }
+    console.log(res);
+    $('#res').text(res);
+    if(res.trim().length == 0){
+        $('#res').text("0");
+    }
+    $('#res').text(res);
+});
